6720f7a7b9fb4dcb7d720b86811591c692a3e0af
Khairul Ashraff <khairul@ashraff.my>
2012-09-18 13:22:48 +0800

    first commit

---
+++ class.folder.php
@@ -0,0 +1,187 @@
+<?php
+class Folder {
+	
+	/*
+	 * total files count
+	 * 
+	 * @access  public
+	 */
+	public $count = 0;
+	
+	
+	/*
+	 * total files size
+	 * 
+	 * @access  public
+	 */
+	public $size = 0;
+	
+	
+	/*
+	 * Files array. Use getFiles() to get it.
+	 * 
+	 * @access  private
+	 */
+	private $files = array();
+	
+	
+	/*
+	 * Directories array. Use getDirs() to get it.
+	 * 
+	 * @access  private
+	 */
+	private $dirs = array();
+	
+	
+	/*
+	 * Current dir's name
+	 * 
+	 * @access  public
+	 */
+	public $name = null;
+	
+	
+	/*
+	 * Current dir. Only set if in inside a sub-directory
+	 * 
+	 * @access  public
+	 */
+	public $current = null;
+	
+	
+	/*
+	 * Up directory. Use getUpURL() for the url
+	 * 
+	 * @access  public
+	 */
+	private $up_dir = null;
+	
+	/*
+	 * constructor
+	 * 
+	 * @access  public
+	 * @return  void
+	 */
+	public function __construct() {
+		$self 		= basename(isset($_SERVER['SCRIPT_FILENAME']) ? $_SERVER['SCRIPT_FILENAME'] : __FILE__);
+		$ignore		= array('.','..','.htaccess','index.php','icon.php','class.folder.php','error.log','customerror.log','Thumbs.db',$self,'css','img','js'); // ignore these files
+
+		$root		= dirname(__FILE__);
+		$dir		= isset($_GET['dir']) ? $_GET['dir'] : '';
+		if(strstr($dir,'..'))
+		{	
+			$dir='';
+		}
+
+		$path		= "$root/$dir/";
+		$dirs		= $files =array();
+
+		if(!is_dir($path) || ($h=opendir($path)) == false)
+		{
+			exit('Directory does not exist.');
+		}
+
+		$this->size = 0;
+		while(false!==($f=readdir($h)))
+		{
+			if(in_array($f,$ignore))
+			{
+				continue;
+			}
+
+			if(is_dir($path.$f))
+			{
+				$this->dirs[] = array(
+							'name'=>$f,
+							'date'=>filemtime($path.$f),
+							'url'=>$self.'?dir='.rawurlencode(trim("$dir/$f",'/'))
+						);
+			}
+			else
+			{
+				$size = filesize($path.$f);
+				$this->files[preg_replace('/[.,_!-\s]/','', $f)] = array(
+																'name'=>$f,
+																'size'=>$size,
+																'date'=>filemtime($path.$f),
+																'url'=>trim("$dir/".rawurlencode($f),'/')
+															);
+				$this->size += $size;
+			}
+		}
+		ksort($this->files);
+		closedir($h);
+		
+		$this->name 	= basename($path);
+		$this->current	= $dir;
+		$this->up_dir	= dirname($dir);		
+		$this->count	= count($this->files) + count($this->dirs);
+	}
+	
+	/*
+	 * Get files array
+	 * 
+	 * @access  public
+	 * @return  Array
+	 */
+	public function getFiles() {
+		return $this->files;
+	}
+	
+	/*
+	 * Get directories array
+	 * 
+	 * @access  public
+	 * @return  Array
+	 */
+	public function getDirs() {
+		return $this->dirs;
+	}
+	
+	/*
+	 * Get URL of upper directory
+	 * 
+	 * @access  public
+	 * @return  String
+	 */
+	public function getUpURL() {
+		$url = ($this->up_dir != '' && $this->up_dir != '.') ? 'index.php?dir=' . rawurlencode($this->up_dir) : 'index.php';
+		
+		return $url;
+	}
+	
+	/*
+	 * Get folder trails
+	 * 
+	 * @access  public
+	 * @return  Array
+	 */
+	public function getTrails() {
+		$trails = explode("/", $this->current);
+		
+		return $trails;
+	}
+
+
+	/*
+	 * Format size from bytes to human-readable format
+	 * 
+	 * @static
+	 * @access  public
+	 * @param  Int  $size  Size to be formatted
+	 * @return  String  Formatted size
+	 */
+	public static function format($size) 
+	{
+		$type = array('B','KB','MB','GB','TB');
+		$count = 0;
+		while($size > 1024) 
+		{
+			$count++;
+			$size /= 1024;
+		}
+
+		return number_format($size, 2) . $type[$count];
+	}
+}
+?>
---
+++ index.php
@@ -0,0 +1,97 @@
+<?php
+$sitename	= 'temp';
+$date		= 'j M Y'; // date format
+
+include_once(dirname(__FILE__) . '/class.folder.php');
+$folder = new Folder();
+
+// END PHP ?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<head>
+<meta http-equiv="Content-type" content="text/html; charset=iso-8859-1" />
+<title><?php echo $sitename ?></title>
+
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<script type="text/javascript" src="js/jquery-1.8.1.min.js"></script>
+<script type="text/javascript" src="js/jquery-icontains.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/stupidtable.min.js"></script>
+<script type="text/javascript" src="js/search.min.js"></script>
+<style>
+	#idx th.sortable, #image-preview img {cursor:pointer;}
+</style>
+
+<script>
+	$(document).ready(function() {
+		var modal = $('#image-preview');
+		
+		$('a').filter(function(){ return /(jpe?g|png|gif)$/i.test($(this).attr('href')); }).click(function(e) {
+			e.preventDefault();
+			modal.modal();
+			var imgSrc = $(this).attr('href');
+			var imgNode = '<img src="'+ imgSrc +'" title="Click to close" rel="tooltip">';
+			modal.html(imgNode);
+			modal.css({'margin' : 0});
+			
+			var img = new Image();
+			img.onload = function() {
+				var left = $(window).width()/2 - this.width/2;
+				var top = $(window).height()/2 - this.height/2;
+				modal.css({'width' : this.width, 'left' : left, 'top': top});
+				$('img', modal).tooltip()
+			}
+			img.src = imgSrc;
+		});
+		
+		$(document).on('click','#image-preview img', function(e) {
+			modal.modal('hide');
+		});
+	});
+</script>
+</head>
+<body>
+	<div class="container" style="padding-top: 30px;">
+		<div class="pull-left">
+			<p class="pull-left">
+				<h3><?php echo $folder->name == '' ? $sitename : $folder->name ?></h3>
+				<p><?php echo $folder->count; ?> objects in this folder, <?php echo Folder::format($folder->size) ?> total.</p>
+			</p>
+			<?php if($folder->current) { ?><i class="icon-chevron-left"></i> &nbsp;<a href="<?php echo $folder->getUpUrl() ?>">Back</a><?php } ?>
+		</div>
+		<div class="pull-right" style="padding-top:50px">
+			<div class="input-prepend input-append">
+				Search: <input type="text" name="search" id="search">
+				<button class="btn" type="button" id="btn-search"><i class="icon-search"></i></button>
+			</div>
+		</div>
+		<table class='table table-striped' id="idx">
+			<thead>
+				<tr>
+					<th class="type-string sortable">Filename</th>
+					<th class="type-int sortable">Size <i class=''></i></th>
+					<th class="type-int sortable">Date <i class=''></i></th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php foreach($folder->getDirs() AS $dir) { ?>
+					<tr>
+						<td data-order-by="<?php echo strtolower($dir['name']) ?>"><i class="icon-folder-close"></i> &nbsp;<a href="<?php echo $dir['url'] ?>"><?php echo $dir['name'] ?></a></td>
+						<td class="span3"></td>
+						<td class="span3" data-order-by="<?php echo $dir['date'] ?>"><?php echo date($date, $dir['date']) ?></td>
+					</tr>
+				<?php } ?>
+				
+				<?php foreach($folder->getFiles() AS $file) { ?>
+				<tr>
+					<td data-order-by="<?php echo strtolower($file['name']) ?>"><i class="icon-file"></i> &nbsp;<a href="<?php echo $file['url'] ?>"><?php echo $file['name'] ?></a></td>
+					<td class="span3" style="width:50px;" data-order-by="<?php echo $file['size'] ?>"><?php echo Folder::format($file['size']) ?></td>
+					<td class="span3" data-order-by="<?php echo $file['date'] ?>"><?php echo date($date, $file['date']) ?></td>
+				</tr>
+				<?php } ?>
+			</tbody>
+		</table>
+	</div>
+	<div class="modal hide fade" id="image-preview">a</div>
+</body>
+</html>
